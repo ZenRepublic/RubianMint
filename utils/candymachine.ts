7334e2d0bf54a309f47de0ffeb71848e2dc44d76
ZenRepublic <hq@zenrepublic.space>
2024-03-16 12:25:36 +0200

    priority fees

--- utils/candymachine.ts
+++ utils/candymachine.ts
@@ -1,50 +1,51 @@
 import {
-    generateSigner,
-    Umi,
-    TransactionBuilder,
-    publicKey,
-    KeypairSigner,
-    sol,
-    some,
-  } from "@metaplex-foundation/umi";
-  import {
-    fetchCandyGuard,
-    fetchCandyMachine,
-    mintV2,
-  } from "@metaplex-foundation/mpl-candy-machine";
-  import {
-    createMintWithAssociatedToken,
-    transferSol,
-    setComputeUnitLimit,
-  } from "@metaplex-foundation/mpl-toolbox";
-  import { TokenStandard } from "@metaplex-foundation/mpl-token-metadata";
-  import bs58 from "bs58";
+  generateSigner,
+  Umi,
+  TransactionBuilder,
+  publicKey,
+  KeypairSigner,
+  sol,
+  some,
+  assertSolAmount,
+} from "@metaplex-foundation/umi"
+import {
+  fetchCandyGuard,
+  fetchCandyMachine,
+  mintV2,
+} from "@metaplex-foundation/mpl-candy-machine"
+import {
+  createMintWithAssociatedToken,
+  transferSol,
+  setComputeUnitLimit,
+  setComputeUnitPrice,
+} from "@metaplex-foundation/mpl-toolbox"
+import { TokenStandard } from "@metaplex-foundation/mpl-token-metadata"
+import bs58 from "bs58"
 
-  const candyMachine = publicKey(
-    "CiAnxgxAtM1pTdNoeZ8nqeTYBM5mPEdQHdwhXfqeS3Th"
-    // "EPDMTLPBkidL1fXamZUXG9Yp4ZSVsg4QpWdPsKuVUx32"
-  );
+const candyMachine = publicKey(
+  "CiAnxgxAtM1pTdNoeZ8nqeTYBM5mPEdQHdwhXfqeS3Th"
+  // "EPDMTLPBkidL1fXamZUXG9Yp4ZSVsg4QpWdPsKuVUx32"
+)
 
-  const tokenMint = publicKey(
-    "ESyHCUfKeT1ffLNRfCsjyHzNL4qN22kruVr8vYkPDR5r"
-    // "devKak33fTumfvB547eEpGn2HYZstHZ9HjpFoLSELVH"
-  );
+const tokenMint = publicKey(
+  "ESyHCUfKeT1ffLNRfCsjyHzNL4qN22kruVr8vYkPDR5r"
+  // "devKak33fTumfvB547eEpGn2HYZstHZ9HjpFoLSELVH"
+)
 
-  const couponMint = publicKey(
-    "2jVEPco6pMAzKG4RijGyarXZcJGuDNZ2ATAjve9dbKc1"
-  );
+const couponMint = publicKey("2jVEPco6pMAzKG4RijGyarXZcJGuDNZ2ATAjve9dbKc1")
 
-  const fundReceiver = publicKey("rbyDXhzsM2xXtV3t9aayYR3md9rY6gyBxbQBeLThtSC");
-  
-  export const createOpenMintTxs = async (umi: Umi) => {
-    const cm = await fetchCandyMachine(umi, candyMachine);
-    const guard = await fetchCandyGuard(umi, cm.mintAuthority)
-    console.log(cm.mintAuthority);
-    const txBuilders: { tx: TransactionBuilder; mint: KeypairSigner }[] = [];
-  
-    const nftMint = generateSigner(umi);
-    const txBuilder = new TransactionBuilder()
-    .add(setComputeUnitLimit(umi, { units: 800000 }))
+const fundReceiver = publicKey("rbyDXhzsM2xXtV3t9aayYR3md9rY6gyBxbQBeLThtSC")
+
+export const createOpenMintTxs = async (umi: Umi) => {
+  const cm = await fetchCandyMachine(umi, candyMachine)
+  const guard = await fetchCandyGuard(umi, cm.mintAuthority)
+  console.log(cm.mintAuthority)
+  const txBuilders: { tx: TransactionBuilder; mint: KeypairSigner }[] = []
+
+  const nftMint = generateSigner(umi)
+  const txBuilder = new TransactionBuilder()
+    .add(setComputeUnitPrice(umi, { lamports: sol(0.005) }))
+    .add(setComputeUnitLimit(umi, { units: 30000000 }))
     // .add(
     //   createMintWithAssociatedToken(umi, {
     //     mint: nftMint,
@@ -52,41 +53,42 @@ import {
     //   })
     // )
     .add(
-        mintV2(umi, {
-        candyMachine:cm.publicKey,
+      mintV2(umi, {
+        candyMachine: cm.publicKey,
         candyGuard: guard.publicKey,
-        collectionMint:cm.collectionMint,
+        collectionMint: cm.collectionMint,
         collectionUpdateAuthority: fundReceiver,
         nftMint: nftMint,
         tokenStandard: TokenStandard.ProgrammableNonFungible,
-        group:some("open"),
+        group: some("open"),
         mintArgs: {
-            solPayment: {
+          solPayment: {
             destination: fundReceiver,
-            },
+          },
         },
-        })
-    );
+      })
+    )
 
-    txBuilders.push({
+  txBuilders.push({
     tx: txBuilder,
     mint: nftMint,
-    });
+  })
 
-    return txBuilders;
-  };
+  return txBuilders
+}
 
-  export const createTokenMintTxs = async (umi: Umi) => {
-    const cm = await fetchCandyMachine(umi, candyMachine);
-    const guard = await fetchCandyGuard(umi, cm.mintAuthority)
-    console.log(cm.mintAuthority);
-    
-    const txBuilders: { tx: TransactionBuilder; mint: KeypairSigner }[] = [];
-  
-    const nftMint = generateSigner(umi);
-  
-    const txBuilder = new TransactionBuilder()
-    .add(setComputeUnitLimit(umi, { units: 800000 }))
+export const createTokenMintTxs = async (umi: Umi) => {
+  const cm = await fetchCandyMachine(umi, candyMachine)
+  const guard = await fetchCandyGuard(umi, cm.mintAuthority)
+  console.log(cm.mintAuthority)
+
+  const txBuilders: { tx: TransactionBuilder; mint: KeypairSigner }[] = []
+
+  const nftMint = generateSigner(umi)
+
+  const txBuilder = new TransactionBuilder()
+    .add(setComputeUnitPrice(umi, { lamports: sol(0.005) }))
+    .add(setComputeUnitLimit(umi, { units: 30000000 }))
     // .add(
     //   createMintWithAssociatedToken(umi, {
     //     mint: nftMint,
@@ -94,39 +96,40 @@ import {
     //   })
     // )
     .add(
-        mintV2(umi, {
-        candyMachine:cm.publicKey,
+      mintV2(umi, {
+        candyMachine: cm.publicKey,
         candyGuard: guard.publicKey,
-        collectionMint:cm.collectionMint,
+        collectionMint: cm.collectionMint,
         collectionUpdateAuthority: fundReceiver,
         nftMint: nftMint,
         tokenStandard: TokenStandard.ProgrammableNonFungible,
-        group:some("token"),
+        group: some("token"),
         mintArgs: {
-            tokenBurn: some({mint: tokenMint}),
+          tokenBurn: some({ mint: tokenMint }),
         },
-        })
-    );
+      })
+    )
 
-    txBuilders.push({
+  txBuilders.push({
     tx: txBuilder,
     mint: nftMint,
-    });
-  
-    return txBuilders;
-  };
+  })
 
-  export const createCouponMintTxs = async (umi: Umi) => {
-    const cm = await fetchCandyMachine(umi, candyMachine);
-    const guard = await fetchCandyGuard(umi, cm.mintAuthority)
-    console.log(cm.mintAuthority);
-    
-    const txBuilders: { tx: TransactionBuilder; mint: KeypairSigner }[] = [];
-  
-    const nftMint = generateSigner(umi);
-  
-    const txBuilder = new TransactionBuilder()
-    .add(setComputeUnitLimit(umi, { units: 800000 }))
+  return txBuilders
+}
+
+export const createCouponMintTxs = async (umi: Umi) => {
+  const cm = await fetchCandyMachine(umi, candyMachine)
+  const guard = await fetchCandyGuard(umi, cm.mintAuthority)
+  console.log(cm.mintAuthority)
+
+  const txBuilders: { tx: TransactionBuilder; mint: KeypairSigner }[] = []
+
+  const nftMint = generateSigner(umi)
+
+  const txBuilder = new TransactionBuilder()
+    .add(setComputeUnitPrice(umi, { lamports: sol(0.005) }))
+    .add(setComputeUnitLimit(umi, { units: 30000000 }))
     // .add(
     //   createMintWithAssociatedToken(umi, {
     //     mint: nftMint,
@@ -134,27 +137,27 @@ import {
     //   })
     // )
     .add(
-        mintV2(umi, {
-        candyMachine:cm.publicKey,
+      mintV2(umi, {
+        candyMachine: cm.publicKey,
         candyGuard: guard.publicKey,
-        collectionMint:cm.collectionMint,
+        collectionMint: cm.collectionMint,
         collectionUpdateAuthority: fundReceiver,
         nftMint: nftMint,
         tokenStandard: TokenStandard.ProgrammableNonFungible,
-        group:some("coupon"),
+        group: some("coupon"),
         mintArgs: {
-            solPayment: {
+          solPayment: {
             destination: fundReceiver,
-            },
-            tokenBurn: some({mint: couponMint}),
+          },
+          tokenBurn: some({ mint: couponMint }),
         },
-        })
-    );
+      })
+    )
 
-    txBuilders.push({
+  txBuilders.push({
     tx: txBuilder,
     mint: nftMint,
-    });
-  
-    return txBuilders;
-  };
+  })
+
+  return txBuilders
+}
